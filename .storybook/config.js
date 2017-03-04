67ac76bfafda15c80a92bff01bdeaa14dabbb6f2
Richard Herrera <richard.herrera@nfl.com>
2017-03-04 08:05:08 -0800

    feat: Add describe alias "context"

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,13 +3,14 @@ import * as storybook from '@kadira/storybook';
 
 //THIS IS NEEDED ONLY IF YOU ARE USING MOCHA AS A TEST RUNNER
 
-import {storiesOf, action, linkTo, specs, describe, it,
+import {storiesOf, action, linkTo, specs, context, describe, it,
 after, before, beforeEach, afterEach} from "./facade";
 
 global.storiesOf = storiesOf;
 global.action = action;
 global.linkTo = linkTo;
 global.specs = specs;
+global.context = context;
 global.describe = describe;
 global.it = it;
 global.after = after;
--- .storybook/facade.js
+++ .storybook/facade.js
@@ -2,7 +2,8 @@ import {storiesOf as storiesOfReal,
     action as actionReal,
 linkTo as linkToReal} from "@kadira/storybook"
 import { specs as specsReal, describe as describeReal,
-  it as itReal, beforeEach as beforeEachReal,
+context as contextReal,
+it as itReal, beforeEach as beforeEachReal,
 before as beforeReal, after as afterReal,
 afterEach as afterEachReal,
 xit as xitReal,
@@ -13,6 +14,7 @@ export const storiesOf = storiesOfReal;
 export const action = actionReal;
 export const linkTo = linkToReal;
 export const specs = specsReal;
+export const context = contextReal;
 export const describe = describeReal;
 export const it = itReal;
 
